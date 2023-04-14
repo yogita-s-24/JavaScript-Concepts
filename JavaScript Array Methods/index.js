b6cd23258c84861c26031654d61c1577cb68ea2c
yogitashete2412 <yogitashete24@gmail.com>
2023-04-14 21:13:26 +0530

    join method added

--- JavaScript Array Methods/index.js
+++ JavaScript Array Methods/index.js
@@ -9,3 +9,32 @@ let num = [1,2,3,34,4];
 let b = num.toString();
 console.log(b);
 
+// Array can hold many values in a single variables.
+//Array Methods:
+//There are Six array methods in the array.
+
+//1.toString()-Convert an array to the string of comma seperator values
+
+//eg. (O/p => 1,2,34,4)
+let num1 = [1,2,3,34,4];
+let b2 = num.toString();
+console.log(b);
+
+
+//2.join()-join all the array elements using seperator 
+let c = num1.join(" _ ");
+console.log(c, typeof c)
+
+let d = num1.join(" and ");
+console.log(d, typeof d)
+
+let e = num1.join(" & ");
+console.log(e, typeof e)
+
+let f = num1.join(" - ");
+console.log(f, typeof f)
+
+//3.
+//4.
+//5.
+//6.
