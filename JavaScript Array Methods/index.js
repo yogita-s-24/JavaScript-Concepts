e44ed71e4f1a972638bc77e17049142f3ef7e896
yogitashete2412 <yogitashete24@gmail.com>
2023-04-14 21:09:47 +0530

    toString() method added

---
+++ JavaScript Array Methods/index.js
@@ -0,0 +1,11 @@
+// Array can hold many values in a single variables.
+//Array Methods:
+//There are Six array methods in the array.
+
+//1.toString()-Convert an array to the string of comma seperator values
+
+//eg. (O/p => 1,2,34,4)
+let num = [1,2,3,34,4];
+let b = num.toString();
+console.log(b);
+
