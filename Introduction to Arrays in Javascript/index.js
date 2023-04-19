af0c6a6e5a2c8c8412e2adb505af9e3a701010cc
yogitashete2412 <yogitashete24@gmail.com>
2023-04-19 08:01:50 +0530

    practice set added

---
+++ Introduction to Arrays in Javascript/index.js
@@ -0,0 +1,22 @@
+//Array is mutable 
+
+const arr = [1,2,3,4,"Hello", null , false ];
+
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);  //It is undefined bcaz it is not exist
+
+console.log("The length of the arr is "+arr.length);
+
+console.log(arr[6]=90);    //addding new value to the array
+console.log(arr[1]=92);    //changing new value to the array
+console.log(typeof arr);  
+
+
+
