2b087d3fe4024d42f2620e0caa9189cb66b437cd
yogitashete2412 <yogitashete24@gmail.com>
2023-04-20 21:22:05 +0530

    JavaScript script tag added

---
+++ JavaScript Script Tag/script.js
@@ -0,0 +1,11 @@
+//  External CSS
+const friendname1 = "Saurabh";
+const friendname2 = "Harshada";
+function App(){
+    console.log(`Hello ${friendname1}`)
+    console.log(`Hello ${friendname2}`)
+}
+
+App();
+
+
