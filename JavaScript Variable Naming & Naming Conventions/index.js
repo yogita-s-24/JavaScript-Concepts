5875307aaa7f53b706edbb6ef501a2d22946d5ec
yogitashete2412 <yogitashete24@gmail.com>
2023-04-21 09:23:18 +0530

    Variable Naming & Naming Convention

---
+++ JavaScript Variable Naming & Naming Conventions/index.js
@@ -0,0 +1,13 @@
+var myName = "Yogita";
+var yourName = "Jacky Roy";
+var my123 = 123;
+var my_name = "Sona";
+
+
+//This is camel case variable name
+var userScore = 1000;
+
+
+//This syntax is not aloude
+//var my name = "Angela";   
+//var var = "HELLO";     
