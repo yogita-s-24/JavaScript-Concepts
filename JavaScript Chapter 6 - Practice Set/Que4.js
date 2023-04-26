a905ba83276d199e0a3b018b98d29b2a82db9ddd
yogitashete2412 <yogitashete24@gmail.com>
2023-04-26 10:29:10 +0530

    Que.4 added

---
+++ JavaScript Chapter 6 - Practice Set/Que4.js
@@ -0,0 +1,15 @@
+//Que.4) WAP to take a  color from user and change the body bgcolor. 
+
+
+    //const number = parseInt(prompt("Enter the value of number"));
+    // if(number > 4){
+    //          location.href = "https://google.com"
+    // }
+
+
+    let color = prompt("Enter the page background color")
+    document.body.style.background = color  
+
+
+
+    //Logic is correct bt this code not support on vs code it's only work on Replit 
