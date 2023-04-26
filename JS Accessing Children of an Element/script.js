1522cb0269d9809849eda029c466ed2f1bed65b4
yogitashete2412 <yogitashete24@gmail.com>
2023-04-26 23:56:06 +0530

    Accessing Children of an Element

---
+++ JS Accessing Children of an Element/script.js
@@ -0,0 +1,12 @@
+console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+console.log(document.body.childNodes)
+
+
+
+
+
+
+
+
+//This is not working on vs code it's working on Replit
