6357f033371bfdfb9e2b87190251cd13b1a045f5
yogitashete2412 <yogitashete24@gmail.com>
2023-04-27 17:10:34 +0530

    clear btn added

--- Using JS (Add Two Numbers App)(Mini Project)/script.js
+++ Using JS (Add Two Numbers App)(Mini Project)/script.js
@@ -6,3 +6,12 @@ function calSum(){
 
     document.getElementById("sum").innerHTML = "Sum :"+sum;
 }
+
+function reset(){
+    const a = document.getElementById("Val1").value=" ";
+    const b = document.getElementById("Val2").value=" ";
+
+    const sum = parseInt(a) + parseInt(b);
+
+    document.getElementById("sum").innerHTML = "Sum :"+sum;
+}
