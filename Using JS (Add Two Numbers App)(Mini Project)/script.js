4b1a57697949b1b52b0a5ab7eece13f1cb79822f
yogitashete2412 <yogitashete24@gmail.com>
2023-04-27 16:33:45 +0530

    javascript added for sum of two numbers

---
+++ Using JS (Add Two Numbers App)(Mini Project)/script.js
@@ -0,0 +1,8 @@
+function calSum(){
+    const a = document.getElementById("Val1").value;
+    const b = document.getElementById("Val2").value;
+
+    const sum = parseInt(a) + parseInt(b);
+
+    document.getElementById("sum").innerHTML = "Sum :"+sum;
+}
