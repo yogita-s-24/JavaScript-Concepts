603f9da3fb08d3973d9e9626b9cdad7904b2104a
yogitashete2412 <yogitashete24@gmail.com>
2023-04-27 17:13:15 +0530

    js modifed

--- Using JS (Add Two Numbers App)(Mini Project)/script.js
+++ Using JS (Add Two Numbers App)(Mini Project)/script.js
@@ -10,8 +10,4 @@ function calSum(){
 function reset(){
     const a = document.getElementById("Val1").value=" ";
     const b = document.getElementById("Val2").value=" ";
-
-    const sum = parseInt(a) + parseInt(b);
-
-    document.getElementById("sum").innerHTML = "Sum :"+sum;
 }
