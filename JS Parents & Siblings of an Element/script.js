075b8d2a926b181ac70308ce37fb6b31ee17b7ed
yogitashete2412 <yogitashete24@gmail.com>
2023-04-28 10:14:49 +0530

    parents and siblings of ele added

--- JS Parents & Siblings of an Element/script.js
+++ JS Parents & Siblings of an Element/script.js
@@ -1,5 +1,12 @@
 console.log(document.body.firstChild)
-a = document.body.firstChild
-console.log(a.parentNode)
-console.log(a.parentElement)
-// console.log(a.firstChild.nextSibling)
+a = document.body.firstChild               //o/p is #text
+console.log(a.parentNode)                  //It's return parent node it is <body>
+console.log(a.parentElement)              //It's also return parent element it is <body>
+
+
+
+
+//not working on vs code
+console.log(a.firstChild.nextSibling)
+
+//It's O/p is <div class="Second">Second</div>
