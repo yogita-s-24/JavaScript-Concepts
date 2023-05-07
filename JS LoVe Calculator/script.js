83293ac3713f1276a99c5b136137791b2c88e7fb
yogitashete2412 <yogitashete24@gmail.com>
2023-05-07 16:38:46 +0530

    LoVe calulator updated

--- JS LoVe Calculator/script.js
+++ JS LoVe Calculator/script.js
@@ -5,17 +5,17 @@ var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;  //1-100
 
 if (loveScore > 70) {
-  alert("Your love Score is " + loveScore + " %." + "You Love each Other like Krishna loves Radha 🤝❤️😍");
-  document.write("Your love Score is " + loveScore + " %." + "You Love each Other like Krishna loves Radha 🤝❤️😍");
-  document.write("❤️" + urname + " & " + urlovename + "❤️" );
+    document.write( urname + "❤️" + " & " + urlovename + "❤️");
+    alert("Your love Score is " + loveScore + " %." + "You Love each Other like Krishna loves Radha 🤝❤️😍");
+    document.write("Your love Score is " + loveScore + " %." + "You Love each Other like Krishna loves Radha 🤝❤️😍");
 }
 if (loveScore > 30 && loveScore <= 70) {
-  alert("Your love Score is " + loveScore + "%.🤗");
-  document.write("Your love Score is " + loveScore + "%.🤗");
-  document.write("❤️" + urname + " & " + urlovename + "❤️" );
+    document.write( urname + "❤️" + " & " + urlovename + "❤️");
+    alert("Your love Score is " + loveScore + "%.🤗");
+    document.write("Your love Score is " + loveScore + "%.🤗");
 }
 if (loveScore <= 30) {
-  alert("Your love score is " + loveScore + "%"," You go together like oil and water.😅");
-  document.write("Your love score is " + loveScore +  "%"," You go together like oil and water.😅");
-  document.write("❤️" + urname + " & " + urlovename + "❤️" );
+    document.write( urname + "❤️" + " & " + urlovename + "❤️");
+    alert("Your love score is " + loveScore + "%.", " You go together like oil and water.😅");
+    document.write("Your love score is " + loveScore + "%.", " You go together like oil and water.😅");
 }
