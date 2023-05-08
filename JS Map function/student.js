a37ea85feb6031284c6f7b84950dc4f0abcc9954
yogitashete2412 <yogitashete24@gmail.com>
2023-05-08 17:27:03 +0530

    map function added

---
+++ JS Map function/student.js
@@ -0,0 +1,14 @@
+const arr = ["Sau","Harshu","Yogii"];
+
+arr.map((stud)=>{
+    console.log(`Hello 👋 ${stud} Welcome Here!!!`)
+})
+
+
+
+function student(stud){
+    console.log(`Hello 👋 ${stud} Welcome Here!!!`)
+}
+
+const arra = ["sau" ,"Oggy" ,"harshu"]
+arra.map(student);
