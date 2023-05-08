f66f31105f81f6d365f0ef658a5a014908cb76cc
yogitashete2412 <yogitashete24@gmail.com>
2023-05-08 17:12:09 +0530

    map function added

---
+++ JS Map function/map.js
@@ -0,0 +1,20 @@
+
+function Hii() {
+    console.log("Hii")
+}
+const arr = [13, 5, 6, 3, 4]
+arr.map(Hii);
+
+//o/p-
+      // Hii
+      // Hii
+      // Hii
+      // Hii
+      // Hii
+
+      function Hi(q) {
+        console.log(`Hii ${p}`);
+    }
+    const arr1 = [13, 25, 126, 13,1, 4]
+    arr1.map(Hi);
+    
---
+++ JS Map function/square.js
@@ -0,0 +1,8 @@
+// function square(num){
+//     console.log(num*num)
+// }
+const arr = [3,4,5,6];
+arr.map((num)=>{
+    console.log(num*num)
+});
+
