742b485a39eb8d10f8b625a1f41b12bd576ae6e1
Yogita Shete <yogitashete24@gmail.com>
2024-02-23 14:03:22 +0530

    comparision operator added

---
+++ JS comparision operators/index.js
@@ -0,0 +1,10 @@
+console.log(null > 0); //false
+console.log(null == 0); //false
+
+
+console.log(null >= 0); //true
+
+
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
