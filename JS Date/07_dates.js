dcc6b72c18f4a5007de4cd1a389e515d3b3d6769
Yogita Shete <yogitashete24@gmail.com>
2024-03-05 23:18:19 +0530

    date example added

---
+++ JS Date/07_dates.js
@@ -0,0 +1,21 @@
+//Dates 
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+
+console.log(myDate.getDate());
+console.log(myDate.getFullYear());
+console.log(myDate.getDay());
+console.log(myDate.getHours());
+console.log(myDate.getTime());
+
+let myCreatedDate = new Date(2023, 0, 23)
+console.log(myCreatedDate.toDateString())
+
+
