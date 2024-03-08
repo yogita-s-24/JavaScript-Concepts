19de619f77b0425c74e2f8cd1a46dd4501b30d00
Yogita Shete <yogitashete24@gmail.com>
2024-03-08 22:45:28 +0530

    find function  added

--- JS Array/array.js
+++ JS Array/array.js
@@ -1,8 +1,20 @@
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 let size = fruits.length;
-console.log(fruits)
-console.log(size)
+console.log(fruits);
+console.log(size);
 
-const newArr = fruits.pop()
-console.log(newArr)
-console.log(fruits)
+const newArr = fruits.pop();
+console.log(newArr);
+console.log(fruits);
+
+
+const numbers = [4, 9, 16, 25, 29];
+let first = numbers.find(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 18;
+}
+
+console.log(numbers);
+
+console.log(first);
