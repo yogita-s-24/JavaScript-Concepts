c5b542865748d231d901f608d6bcd97bad0e2950
Yogita Shete <yogitashete24@gmail.com>
2024-03-14 22:00:29 +0530

    js function added

--- Javascript Functions/function.js
+++ Javascript Functions/function.js
@@ -1,5 +1,38 @@
-function formatNumber(num) {
-    return num.toFixed(2);
-  }
-  
-  formatNumber(2);
+function sayHello() {
+  console.log("H");
+  console.log("I");
+  console.log("T");
+  console.log("S");
+  console.log("H");
+}
+
+// sayHello();
+// sayHello();
+
+// function additionOfTwo(num1, num2){
+//   console.log(num1 + num2)
+// }
+// additionOfTwo(3, 4);
+
+// function additionofTwo(num1, num2){
+//   console.log(num1 + num2)
+// }
+
+function additionofTwoNum(num1, num2) {
+  // let result = num1 + num2;
+  // return result;
+  return num1 + num2;
+}
+const result = additionofTwoNum(3, 8);
+console.log("Result : ", result);
+
+
+function loginedInUser(username){
+  return `Hey! ${username} just logged.`
+}
+console.log(loginedInUser("Sona"));
+
+function loginedInUser(username){
+  return `Hey! ${username} just logged.`
+}
+console.log(loginedInUser("Sona"));
