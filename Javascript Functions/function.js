1bf445fa8ee5e59ed0e41a3405926ba16e8e5fa4
Yogita Shete <yogitashete24@gmail.com>
2024-03-16 20:17:55 +0530

    function object added

--- Javascript Functions/function.js
+++ Javascript Functions/function.js
@@ -24,15 +24,66 @@ function additionofTwoNum(num1, num2) {
   return num1 + num2;
 }
 const result = additionofTwoNum(3, 8);
-console.log("Result : ", result);
+// console.log("Result : ", result);
 
-
-function loginedInUser(username){
-  return `Hey! ${username} just logged.`
+function loginedInUser(username) {
+  return `Hey! ${username} just logged.`;
 }
-console.log(loginedInUser("Sona"));
+// console.log(loginedInUser("Sona"));
 
-function loginedInUser(username){
-  return `Hey! ${username} just logged.`
+function loginedInUser(username) {
+  return `Hey! ${username} just logged.`;
 }
-console.log(loginedInUser("Sona"));
+// console.log(loginedInUser("Sona"));
+
+// function calculateCardPrice(num1){
+//   return num1
+
+// }
+// console.log(calculateCardPrice(200,300,344));
+
+//op : 200
+
+function calculateCardPrice(...num2) {
+  return num2;
+}
+// console.log(calculateCardPrice(200, 300, 400));
+
+//op : [200, 300, 400]
+
+function calculateCardPrice(val1, val2, ...num3) {
+  // return num3
+  // return val1
+  return val2;
+}
+// console.log(calculateCardPrice(200, 300, 400, 900, 1000));
+
+//op : [200, 300, 400]
+
+const user = {
+  userName: "Yogii",
+  price: 199,
+};
+
+
+function handleObject(associtedObj) {
+  // console.log(
+  //   `Username is ${associtedObj.userName} and price the product is ${associtedObj.price}.`
+  // );
+}
+
+// handleObject(user);
+handleObject({
+  userName: "Yogii",
+  price: 199,
+});
+
+
+const newArray = [200, 400, 600, 800];
+
+function returnSecondArray(getArray){
+  return getArray[3]
+}
+
+// console.log(returnSecondArray(newArray));
+console.log(returnSecondArray([200, 400, 600, 800]));
