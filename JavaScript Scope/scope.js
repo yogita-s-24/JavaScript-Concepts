2608beba70eb7e37f01b94a3a436ab60124fd8d3
Yogita Shete <yogitashete24@gmail.com>
2024-03-18 15:21:41 +0530

    javascript scope level

---
+++ JavaScript Scope/scope.js
@@ -0,0 +1,23 @@
+// let a=10;
+// const b=20;
+// var c=30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//{} - It is  known as scope
+//In the {} its a block scope and outside the {} its a global scope.
+if (true) {
+  let a = 10;
+  const b = 20;
+  var c = 30;
+  console.log(a);
+  console.log(b);
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+// var keyword is the global scope variable bt let and const both are block scope variables
