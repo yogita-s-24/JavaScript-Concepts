5ce945980c18a84ee43030b1cddfb3726ded62d2
Yogita Shete <yogitashete24@gmail.com>
2024-03-29 23:26:33 +0530

    comment and example added

---
+++ 26_JS_Control_Flow/switch.js
@@ -0,0 +1,19 @@
+const month = 1;
+
+switch(month){
+    case 1 : 
+    console.log("Jan");
+    break;
+    case 2 : 
+    console.log("Feb");
+    break;
+    case 3 : 
+    console.log("Mar");
+    break;
+    case 4 : 
+    console.log("Apr");
+    break;
+    case 5 : 
+    console.log("May");
+    break;
+}
--- JS This keyword and arrow fun/this.js
+++ JS This keyword and arrow fun/this.js
@@ -7,9 +7,9 @@ const user = {
     // console.log(this);
   },
 };
-// user.welcomeMessage();
-// user.userName = "Samy";
-// user.welcomeMessage();
+user.welcomeMessage();
+user.userName = "Samy";
+user.welcomeMessage();
 
 // console.log(this);
 
@@ -38,5 +38,7 @@ console.log(addTwoString("Hii","Nikhil"))
 // When we can pass parenthesis around the object, we can print object - output is { userName: 'Yogii' }
 
 const addTwoNo = (num1, num2) => {userName : "Yogii"}
-
+ 
 console.log(addTwoNo());
+
+// Today, I learned about the "this" keyword in JavaScript. The behavior of the "this" keyword in functions is unique compared to other programming languages. It depends on how the function is called and can change each time the function is used. It is not possible to change the value of "this" directly while the function is running. However, we can use the "bind()" method to set the value of "this" for a function, regardless of how it is called. Arrow functions maintain the value of "this" from where they are defined, instead of creating their own context.
