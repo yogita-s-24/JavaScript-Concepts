b2ff772137a0c87b671a94b06d16365806596ea3
Yogita Shete <yogitashete24@gmail.com>
2024-03-31 23:59:32 +0530

    truthy value ex added

---
+++ 26_JS_Control_Flow/truthy.js
@@ -0,0 +1,12 @@
+const userEmail = "yogi@gmail.com"
+
+if(userEmail){
+    console.log("Got user email.")
+}
+else{
+    console.log("Don't have user email.")
+}
+
+//falsy values 
+
+//false, 0, -0, BigInt On, " ", null, undefined, NaN
