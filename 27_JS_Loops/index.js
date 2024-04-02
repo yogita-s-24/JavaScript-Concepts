e28f6832d05d9994dc88009a41949caafb815b0c
Yogita Shete <yogitashete24@gmail.com>
2024-04-02 22:55:55 +0530

    for loop in js

---
+++ 27_JS_Loops/index.js
@@ -0,0 +1,13 @@
+//for
+
+// for(let index=1; index<=10; i++){
+//     const element = i;
+//     console.log(element)
+// }
+
+
+for(let i=1; i<=10; i++){
+    const element = i;
+    console.log(element)
+}
+
