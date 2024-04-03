633149061a232674d946bb22b425ef7e6954ab87
Yogita Shete <yogitashete24@gmail.com>
2024-04-03 23:53:16 +0530

    for loop example added

--- 27_JS_Loops/index.js
+++ 27_JS_Loops/index.js
@@ -11,3 +11,11 @@ for(let i=1; i<=10; i++){
     console.log(element)
 }
 
+
+for (let i = 0; i < 10; i++) {
+  console.log(`Outer loop value : ${i}`);
+  for(let j=0; j<=10; j++){
+    console.log(`Inner loop value ${j} and inner loop ${i}`);
+  } 
+  
+}
