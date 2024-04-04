d929ac105d5ec8d3afd8f71ea7e0ecae4d6cd5b1
Yogita Shete <yogitashete24@gmail.com>
2024-04-04 22:37:14 +0530

    tables printed with for loop

--- 27_JS_Loops/index.js
+++ 27_JS_Loops/index.js
@@ -19,3 +19,17 @@ for (let i = 0; i < 10; i++) {
   } 
   
 }
+
+// for (let i = 1; i < array.length; i++) {
+//   console.log(`Outer loop value : ${i}`);
+//   for(let j=0; j<=10; j++){
+//     console.log(`Inner loop value ${j} and inner loop ${i}`);
+//   } 
+// }
+
+for (let i = 1; i < 10; i++) {
+  console.log(`Outer loop value : ${i}`);
+  for(let j=1; j<=10; j++){
+    console.log(` ${i} * ${j} = ${i * j}`);
+  } 
+}
