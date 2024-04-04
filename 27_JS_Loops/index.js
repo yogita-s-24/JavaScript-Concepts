03eccfff56625d5509bbdee06886b3b33dd5c029
Yogita Shete <yogitashete24@gmail.com>
2024-04-04 23:18:59 +0530

    break and continue example added

--- 27_JS_Loops/index.js
+++ 27_JS_Loops/index.js
@@ -33,3 +33,28 @@ for (let i = 1; i < 10; i++) {
     console.log(` ${i} * ${j} = ${i * j}`);
   } 
 }
+
+
+const myArray = ['Sonu', 'Aaru', 'Ira'] 
+console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  console.log(element);
+}
+
+for (let i = 1; i <= 20; i++) {
+  if(i == 5){
+    console.log(`Detected 5`);
+    break
+  }
+  console.log(`Value of i is ${i}`);
+  
+}
+for (let i = 1; i <= 20; i++) {
+  if(i == 5){
+    console.log(`Detected 5`);
+    continue
+  }
+  console.log(`Value of i is ${i}`);
+  
+}
