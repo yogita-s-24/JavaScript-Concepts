190559e255930ecf7c2737fbee4b33426f164c33
Yogita Shete <yogitashete24@gmail.com>
2024-04-10 20:54:38 +0530

    new example added

--- 30_JS_filter_map_reduce/index.js
+++ 30_JS_filter_map_reduce/index.js
@@ -1,11 +1,11 @@
 const coding = ['js', 'ruby', 'java', 'python', 'cpp']
 
 const value = coding.forEach((item)=>{
-    console.log(item);
+    // console.log(item);
     // return item
 })
 
-console.log(value);
+// console.log(value);
 
 
 const myNum = [1,24,5,3,4,5,6,7,8];
@@ -14,4 +14,13 @@ const newNums = myNum.filter((num)=>{
     return num > 4
 })
 
-console.log(newNums);
+// console.log(newNums);
+
+
+const myNumber = [12,24,5,32,4,53,6,7,8];
+
+const newNumbers = myNumber.filter((num)=>{
+    return num % 2 
+})
+
+console.log(newNumbers);
