1d4fed4eca7612d4a749f2a41162cf48083ebb8c
Yogita Shete <yogitashete24@gmail.com>
2024-09-20 19:33:05 +0530

    Combine arrays and strings using spread operator

---
+++ Code_wars/spread.js
@@ -0,0 +1,12 @@
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const combined = [...arr1, ...arr2];
+console.log(combined); // Output: [1, 2, 3, 4, 5, 6]
+
+
+
+const str1 = "hhdfkdsjhkjshfjhsjhfjhf";
+const str2 = "hjdsfkjhdsfhkjahfhshfd";
+
+const bothString = [...str1, ...str2]
+console.log(bothString);
