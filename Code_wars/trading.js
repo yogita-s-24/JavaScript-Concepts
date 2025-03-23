b4482d9c273ca212339c042ec419a22d2e36f134
Yogita Shete <yogitashete24@gmail.com>
2025-03-23 22:50:12 +0530

    add trading example

---
+++ Code_wars/trading.js
@@ -0,0 +1,8 @@
+function maxProfit(prices) {
+    let minPrice = Infinity, maxProfit = 0;
+    for (let price of prices) {
+        minPrice = Math.min(minPrice, price);
+        maxProfit = Math.max(maxProfit, price - minPrice);
+    }
+    return maxProfit;
+}
