ab85804691a2a0c89d56efa508901712193d13a4
Yogita Shete <yogitashete24@gmail.com>
2025-04-17 22:28:04 +0530

    feat: add quote function to return legendary UFC fighter statements based on winner

---
+++ Code_wars/fighter.js
@@ -0,0 +1,10 @@
+function quote(fighter) {
+    if (fighter.toLowerCase() === "george saint pierre") {
+      return "I am not impressed by your performance.";
+    } else if (fighter.toLowerCase() === "conor mcgregor") {
+      return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+    } else {
+      return "Unknown fighter";
+    }
+  }
+  
